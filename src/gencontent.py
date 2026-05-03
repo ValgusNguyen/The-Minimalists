2593eab53ef09c1f41811b36ab10e76b31dd8785
Valgus <khuenguyenhaminh@gmail.com>
2026-05-04 01:01:10 +0700

    chore: deploy fix

--- src/gencontent.py
+++ src/gencontent.py
@@ -3,9 +3,19 @@ from pathlib import Path
 from markdown_blocks import markdown_to_html_node
 
 
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
+    for filename in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        if os.path.isfile(from_path):
+            dest_path = Path(dest_path).with_suffix(".html")
+            generate_page(from_path, template_path, dest_path, basepath)
+        else:
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
+
+
 def generate_page(from_path, template_path, dest_path, basepath):
     print(f" * {from_path} {template_path} -> {dest_path}")
-    
     from_file = open(from_path, "r")
     markdown_content = from_file.read()
     from_file.close()
@@ -23,6 +33,12 @@ def generate_page(from_path, template_path, dest_path, basepath):
     template = template.replace('href="/', 'href="' + basepath)
     template = template.replace('src="/', 'src="' + basepath)
 
+    dest_dir_path = os.path.dirname(dest_path)
+    if dest_dir_path != "":
+        os.makedirs(dest_dir_path, exist_ok=True)
+    to_file = open(dest_path, "w")
+    to_file.write(template)
+
 
 def extract_title(md):
     lines = md.split("\n")
@@ -30,14 +46,3 @@ def extract_title(md):
         if line.startswith("# "):
             return line[2:]
     raise ValueError("no title found")
-
-
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
-    for filename in os.listdir(dir_path_content):
-        from_path = os.path.join(dir_path_content, filename)
-        dest_path = os.path.join(dest_dir_path, filename)
-        if os.path.isfile(from_path):
-            dest_path = os.path.splitext(dest_path)[0] + ".html"
-            generate_page(from_path, template_path, dest_path, basepath)
-        else:
-            generate_pages_recursive(from_path, template_path, dest_path, basepath)
